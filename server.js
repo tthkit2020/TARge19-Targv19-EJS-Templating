f5b66c3d8207d4ae504ac701db98acb792cd3e09
Julia Aasmaa <my_e_10@hotmail.com>
2020-09-30 15:19:08 +0300

    Add JS Scriptlets

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 
+app.use(express.static('public'));
 app.set('view engine', ejs);
+
 
 app.get('/date', (req, res) => {
     let today = new Date();
     let day = '';
+    let personData = {
+                firstName: "John",
+                lastName: "Doe",
+                age: 34,
+                job: "Developer"
+                };
 
     if(today.getDay() === 6 || today.getDate() === 0){
         day = 'Weekend!';
     } else {
         day = 'Workday';
     }
-    res.render('index.ejs', {kindOfDay: day});
+    res.render('index.ejs', {kindOfDay: day,
+                            person: personData
+                            });
 });
 
 
